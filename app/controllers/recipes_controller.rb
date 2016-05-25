b2337a31f3000f5ed280d6c3aadd4451a04dee66
Tom Hernandez <tomhernandez@gmail.com>
2016-05-25 20:32:55 +0000

    add pagination

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,7 +1,7 @@
 class RecipesController < ApplicationController
 
   def index
-    @recipes = Recipe.all
+    @recipes = Recipe.paginate(page: params[:page], per_page: 4)
   end
 
   def show
@@ -14,6 +14,7 @@ class RecipesController < ApplicationController
 
   def create
     @recipe = Recipe.new(recipe_params)
+    #Hardcoding chef for now
     @recipe.chef = Chef.find(2)
     if @recipe.save
       flash[:success] = "Your recipe was created successfully!"
@@ -35,6 +36,19 @@ class RecipesController < ApplicationController
     else
       render :edit
     end
+  end
+
+  def like
+      @recipe = Recipe.find(params[:id])
+      #Hardcoding chef for now
+      like = Like.create(like: params[:like], chef: Chef.first, recipe: @recipe)
+      if like.valid?
+          flash[:success] = "Your selection was successful"
+          redirect_to :back
+      else
+          flash[:danger] = "You can only like/dislike a recipe once"
+          redirect_to :back
+      end
   end
 
 
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,5 +1,6 @@
 class Recipe < ActiveRecord::Base
   belongs_to :chef
+  has_many   :likes
 
   validates :chef_id,     presence: true
   validates :name,        presence: true, length: { minimum: 5,  maximum: 100 }
@@ -7,11 +8,23 @@ class Recipe < ActiveRecord::Base
   validates :description, presence: true, length: { minimum: 20, maximum: 500 }
 
   mount_uploader :picture, PictureUploader
+    # To impliment ordering by number of likes, will have to
+    # add a column to the recipes table, and increment every time
+    # the like button is pressed, then update this method to order
+    # by that column
+  default_scope -> { order(updated_at: :desc) }
 
   validate :picture_size
+  def thumbs_up_total
+      self.likes.where(like: true).size
+  end
+
+  def thumbs_down_total
+      self.likes.where(like: false).size
+  end
 
   private
-  
+
     def picture_size
         if picture.size > 5.megabytes
             errors.add(:picture, "should be less than 5MB")

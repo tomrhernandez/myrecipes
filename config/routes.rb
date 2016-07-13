651a9bd67d1af1a3cd98dcd9513f070a337ebee8
Tom Hernandez <tomhernandez@gmail.com>
2016-07-13 19:59:36 +0000

    Add admin functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,17 @@
 Rails.application.routes.draw do
 
   root 'pages#home'
+
   get '/home', to: 'pages#home'
+
   resources :recipes do
       member do
           post 'like'
       end
   end
-  resources :chefs, except: [:new]
+
+  resources :chefs, except: [:new, :destroy]
+
   get  '/register', to: 'chefs#new'
   get  '/login',    to: 'logins#new'
   post '/login',    to: 'logins#create'
